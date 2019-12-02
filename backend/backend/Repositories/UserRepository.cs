9dea2bee93712b42606393ec60f46c2281e6d3a3
kristiyanyanchev <kristiyanyanchev@abv.bg>
2019-12-02 18:42:59 +0200

    Minor changes

--- backend/backend/Repositories/UserRepository.cs
+++ backend/backend/Repositories/UserRepository.cs
@@ -1,4 +1,33 @@
-$HEADER$namespace $NAMESPACE$
+using System.Collections.Generic;
+using backend.Models;
+
+namespace backend.Repositories
 {
-  public class $CLASS$ {$END$}
+    public class UserRepository : IRepository<User>
+    {
+        public void Add(User value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(User value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public User GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Edit(User value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
 }
