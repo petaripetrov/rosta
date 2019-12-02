41a1e39691678b44f3a49eb2d9b129c8dd1507e9
Петър Петров <petizad55@gmail.com>
2019-12-02 23:05:19 +0200

    Initial library set up

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCoffee, faBars } from '@fortawesome/free-solid-svg-icons';
+
 import './index.css';
 import App from './App';
+import Header from './Components/header'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+library.add(faCoffee, faBars);
+
+
+ReactDOM.render(
+    <div>
+        <Header />
+        <App />
+    </div>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
