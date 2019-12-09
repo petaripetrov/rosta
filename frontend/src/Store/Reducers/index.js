c25d347317dbcf4697a817d29ea8efd738315983
Петър Петров <petizad55@gmail.com>
2019-12-09 22:01:41 +0200

    Router setup and giving it access to the store

--- frontend/src/Store/Reducers/index.js
+++ frontend/src/Store/Reducers/index.js
@@ -1,8 +1,9 @@
+import { connectRouter } from 'connected-react-router'
 import { combineReducers } from 'redux'
 import { burgerReducer } from './Burger'
 
-const createRootReducer = () => combineReducers({
-    // router: 'none',
+const createRootReducer = (history) => combineReducers({
+    router: connectRouter(history),
     burger: burgerReducer,
     // other reducers go here with naming convention | name: nameRouter
 })
--- frontend/src/Store/index.js
+++ frontend/src/Store/index.js
@@ -1,14 +1,20 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
+import { createBrowserHistory } from 'history'
+import { routerMiddleware } from 'connected-react-router'
 
 import createRootReducer from './Reducers'
 
+export const history = createBrowserHistory()
+
 function configureStore(preloadedState: any) {
     const store = createStore(
-        createRootReducer(),
+        createRootReducer(history),
         preloadedState,
         compose(
-            applyMiddleware(thunkMiddleware),
+            applyMiddleware(
+                thunkMiddleware,
+                routerMiddleware(history)),
             window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
         )
     )
