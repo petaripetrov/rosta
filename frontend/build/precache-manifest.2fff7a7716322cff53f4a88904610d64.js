a8b0ed9911f3876726a24a5b53ce81ad32d7749b
Петър Петров <petizad55@gmail.com>
2019-12-14 21:24:41 +0200

    Only build folder should be in production

---
+++ frontend/build/precache-manifest.2fff7a7716322cff53f4a88904610d64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edb3bfd01d0e5d827cd87391bb5b6e4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11ca1355f77fffbcd1d6",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "4236f8018abcbb17a0a3",
+    "url": "/static/css/main.0c36c2ac.chunk.css"
+  },
+  {
+    "revision": "11ca1355f77fffbcd1d6",
+    "url": "/static/js/2.810faa49.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.810faa49.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4236f8018abcbb17a0a3",
+    "url": "/static/js/main.8d59d6e4.chunk.js"
+  },
+  {
+    "revision": "810b7eddbba2845a5756",
+    "url": "/static/js/runtime-main.4e64986e.js"
+  }
+]);
--- frontend/src/Store/Actions/Burger.js
+++
@@ -1,9 +0,0 @@
-export const BURGER_TURN = 'BURGER_TURN'
-export const BURGER_OFF = 'BURGER_OFF'
-
-export function burgerTurn(myRef) {
-    return { 
-        type: BURGER_TURN,
-        ref: myRef
-    }
-}
