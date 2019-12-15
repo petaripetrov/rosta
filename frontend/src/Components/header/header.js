f5a8f2077d249f364303bdb630632814ded5d049
Петър Петров <petizad55@gmail.com>
2019-12-15 18:32:45 +0200

    header and burger now use hooks instead of primitively passing values through props. The animation is also kept in the component that actually uses it, which greatly increses code inversion of control

--- frontend/src/Components/header/header.js
+++ frontend/src/Components/header/header.js
@@ -1,40 +1,39 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Button, Container } from 'react-bootstrap'
 import anime from 'animejs'
 import { BurgerNav } from "./burger"
 
-export class Header extends React.Component {
-    state = { alteredPosition: true }
-    myRef = React.createRef()
+export function Header() {
+    const myRef = React.createRef()
+    const dispatch = useDispatch()
 
-    render() {
-        return (
-            <div className="header" >
-                <Button ref={this.myRef} className="burger"
-                    onClick={() => {
-                        this.setState({ alteredPosition: !this.state.alteredPosition, })
-                        anime({
-                            targets: this.myRef.current,
-                            backgroundPositionX: '0px',
-                            scale: {
-                                value: [1, 0.8],
-                                duration: 1000
-                            },
-                            scale:{
-                                value: [0.8, 1],
-                                duration: 1000
-                            }
-                        })
+    return (
+        <div className="header" >
+            <Button ref={myRef} className="burger"
+                onClick={() => {
+                    dispatch({ type: 'BURGER_TURN' })
+                    anime({
+                        targets: myRef.current,
+                        backgroundPositionX: '0px',
+                        scale: {
+                            value: [1, 0.8],
+                            duration: 1000
+                        },
+                        scale: {
+                            value: [0.8, 1],
+                            duration: 1000
+                        }
+                    })
                 }
                 }>
-                    <FontAwesomeIcon icon="bars" />
-                </Button>
+                <FontAwesomeIcon icon="bars" />
+            </Button>
             <BurgerNav />
             <Container className="schoolName">
                 {'School name goes here through API call'}
             </Container>
-            </div >
-        )
-    }
+        </div >
+    )
 }
