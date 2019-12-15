cd2e5f1515cd9cf28eac59727423af4cc11208e7
Петър Петров <petizad55@gmail.com>
2019-12-15 18:30:38 +0200

    cleaned up the reducer and action as they no longer need to hold a reference to the object and the animation

--- frontend/src/Store/Actions/Burger.js
+++ frontend/src/Store/Actions/Burger.js
@@ -1,9 +1,8 @@
 export const BURGER_TURN = 'BURGER_TURN'
 export const BURGER_OFF = 'BURGER_OFF'
 
-export function burgerTurn(myRef) {
+export function burgerTurn() {
     return { 
         type: BURGER_TURN,
-        ref: myRef
     }
 }
--- frontend/src/Store/Reducers/Burger.js
+++ frontend/src/Store/Reducers/Burger.js
@@ -7,26 +7,20 @@ export const burgerReducer = (state = [], action, burger) => {
     switch (action.type) {
         case 'BURGER_TURN':
             if (isHidden) {
-                basicTimeline
-                    .add({
-                        targets: action.ref.current,
-                        duration: 400,
-                        translateX: 300,
-                        easing: 'easeInOutQuad'
-                    })
                 isHidden = false
-                return state
+
+                return {
+                    ...state,
+                    burgerTurn: !isHidden
+                }
             } else {
-                basicTimeline
-                    .add({
-                        targets: action.ref.current,
-                        duration: 400,
-                        translateX: 0,
-                        easing: 'easeInOutQuad'
-                    })
+
                 isHidden = true
 
-                return state
+                return {
+                    ...state,
+                    burgerTurn: !isHidden
+                }
 
             }
         default:
