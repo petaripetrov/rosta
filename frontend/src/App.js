cca3714418a9f9a3d63f4edb326764a5c9cce93c
Petar <35622776+petaripetrov@users.noreply.github.com>
2019-12-25 15:11:00 +0200

    Refractoring

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,22 @@
-import React from 'react';
+import React from 'react'
 import { Landing } from './Pages/Landing/landing'
 import { LoginRegister } from './Pages/LoginRegister/loginRegister'
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { ConnectedRouter } from 'connected-react-router';
-import { history } from './Store'
-
+import { Switch, Route } from 'react-router-dom'
 
 function App() {
 
   return (
-    <ConnectedRouter history={history}>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/">
-            <Landing />
-          </Route>
-          <Route exact path="/login">
-            <LoginRegister type="login" />
-          </Route>
-          <Route exact path="/register">
-            <LoginRegister type="register" />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </ConnectedRouter>
+    <Switch>
+      <Route exact path="/">
+        <Landing />
+      </Route>
+      <Route exact path="/login">
+        <LoginRegister type="login" />
+      </Route>
+      <Route exact path="/register">
+        <LoginRegister type="register" />
+      </Route>
+    </Switch>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,22 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faCoffee, faBars } from '@fortawesome/free-solid-svg-icons';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Provider } from 'react-redux';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee, faBars } from '@fortawesome/free-solid-svg-icons'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import { ConnectedRouter } from 'connected-react-router'
 
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import store from './Store';
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import store, { history } from './Services/Store'
 import { Header } from './Components'
+
 
 library.add(faCoffee, faBars);
 
 ReactDOM.render(
     <Provider store={store}>
-        <Header />
-        <App />
+        <ConnectedRouter history={history}>
+            <BrowserRouter>
+                <Header />
+                <App />
+            </BrowserRouter>
+        </ConnectedRouter>
     </Provider>,
     document.getElementById('root')
 );
