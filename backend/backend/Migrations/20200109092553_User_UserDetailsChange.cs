37ba66422cc667ef9088e643bcae214e7d360683
kristiyanyanchev <kristiyanyanchev@abv.bg>
2020-01-15 08:11:01 +0200

    Adding SignOut Controller

---
+++ backend/backend/Migrations/20200109092553_User_UserDetailsChange.cs
@@ -0,0 +1,202 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace backend.Migrations
+{
+    public partial class User_UserDetailsChange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Candidacies_Users_OwnerId",
+                table: "Candidacies");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Surveys_Users_AuthorId",
+                table: "Surveys");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Votes_Options_OptionId",
+                table: "Votes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Votes_Surveys_SurveyId",
+                table: "Votes");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SurveyId",
+                table: "Votes",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OptionId",
+                table: "Votes",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "UserDetails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    PhotoPath = table.Column<string>(nullable: true),
+                    UserId = table.Column<int>(nullable: false),
+                    SchoolId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserDetails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserDetails_Schools_SchoolId",
+                        column: x => x.SchoolId,
+                        principalTable: "Schools",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserDetails_SchoolId",
+                table: "UserDetails",
+                column: "SchoolId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Candidacies_UserDetails_OwnerId",
+                table: "Candidacies",
+                column: "OwnerId",
+                principalTable: "UserDetails",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Surveys_UserDetails_AuthorId",
+                table: "Surveys",
+                column: "AuthorId",
+                principalTable: "UserDetails",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Votes_Options_OptionId",
+                table: "Votes",
+                column: "OptionId",
+                principalTable: "Options",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Votes_Surveys_SurveyId",
+                table: "Votes",
+                column: "SurveyId",
+                principalTable: "Surveys",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Candidacies_UserDetails_OwnerId",
+                table: "Candidacies");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Surveys_UserDetails_AuthorId",
+                table: "Surveys");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Votes_Options_OptionId",
+                table: "Votes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Votes_Surveys_SurveyId",
+                table: "Votes");
+
+            migrationBuilder.DropTable(
+                name: "UserDetails");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SurveyId",
+                table: "Votes",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OptionId",
+                table: "Votes",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    AuthenticationCode = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Email = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Password = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    PhotoPath = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    Role = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    SchoolId = table.Column<int>(type: "int", nullable: true),
+                    Username = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_Schools_SchoolId",
+                        column: x => x.SchoolId,
+                        principalTable: "Schools",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_SchoolId",
+                table: "Users",
+                column: "SchoolId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Candidacies_Users_OwnerId",
+                table: "Candidacies",
+                column: "OwnerId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Surveys_Users_AuthorId",
+                table: "Surveys",
+                column: "AuthorId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Votes_Options_OptionId",
+                table: "Votes",
+                column: "OptionId",
+                principalTable: "Options",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Votes_Surveys_SurveyId",
+                table: "Votes",
+                column: "SurveyId",
+                principalTable: "Surveys",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
---
+++ backend/backend/Migrations/20200112144040_UserDetails_Id_Chnaged_Tyoe.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace backend.Migrations
+{
+    public partial class UserDetails_Id_Chnaged_Tyoe : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "UserDetails",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "UserDetails",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
