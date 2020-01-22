a4959ac1e5b9405c971de8f9be1b9965cb57ef66
kristiyanyanchev <kristiyanyanchev@abv.bg>
2020-01-22 20:27:33 +0200

    Option and Survey Changes

---
+++ backend/backend/DTOs/OptionDTOs/IOptionInput.cs
@@ -0,0 +1,7 @@
+namespace backend.DTOs.OptionDTOs
+{
+    public interface IOptionInput
+    {
+        
+    }
+}
---
+++ backend/backend/DTOs/OptionDTOs/OptionToNewSurveyInput.cs
@@ -0,0 +1,7 @@
+namespace backend.DTOs.OptionDTOs
+{
+    public class OptionToNewSurveyInput
+    {
+        
+    }
+}
