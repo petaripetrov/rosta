48039c0fc0ae9bc096e5ada39625dd3477f13d7a
Petar <35622776+petaripetrov@users.noreply.github.com>
2020-02-23 20:04:16 +0200

    Final Changes

--- frontend/src/Pages/Admin Panel/adminPanel.tsx
+++ frontend/src/Pages/Admin Panel/adminPanel.tsx
@@ -1,46 +1,42 @@
-import React, { useState, useEffect, useRef } from 'react'
-import useAPI from '../../Services/API'
+import React, { useState, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useDispatch, useSelector } from 'react-redux'
+import './adminPanel.css'
 
 interface AdminPanelProps {
-    schoolId: string | undefined
+    schools: Array<any> | undefined
 }
 
 interface ModalState {
     active?: string
     user?: any
+
+}
+
+interface ModalRequest {
+    role?: string
+    school?: any
+}
+
+interface SchoolState {
+    schoolId?: string
+    schoolName?: string
 }
 
 export const AdminPanel = (props: AdminPanelProps) => {
     const dispatch = useDispatch()
     const authCode = useSelector((state: any) => state.user.authCode)
-    const [modalState, setModalState] = useState<ModalState>({ active: undefined, user: undefined })
+    const [modalState, setModalState] = useState<ModalState>()
     const [users, setUsers] = useState()
+    const [schoolState, setSchoolState] = useState<SchoolState>()
+    const [modalRequest, setModalRequest] = useState<ModalRequest>()
     const { t } = useTranslation()
-    const modalRef = useRef(null)
-    useOutside(modalRef)
 
-    function useOutside(ref: any) {
-
-        function handleClickOutside(event: any) {
-            if (ref.current && !ref.current.contains(event.target) && modalState) {
-                setModalState({ active: undefined })
-            }
-        }
-
-        useEffect(() => {
-            document.addEventListener("mousedown", handleClickOutside)
-            return () => {
-                document.removeEventListener("mousedown", handleClickOutside)
-            }
-        })
-    }
-
-    useEffect(handleUpdate, [props.schoolId])
+    useEffect(handleUpdate, [schoolState?.schoolId])
+    useEffect(() => { console.log(modalRequest) }, [modalRequest])
 
     function handleUpdate() {
-        fetch(`https://localhost:44375/getAllUsers/${props.schoolId}`, {
+        fetch(`https://localhost:44375/getAllUsers/${schoolState?.schoolId}`, {
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${authCode}`
@@ -66,8 +62,54 @@ export const AdminPanel = (props: AdminPanelProps) => {
             })
     }
 
+    function handleModalSubmit() {
+        fetch('https://localhost:44375/addToSchool', {
+            method: 'POST',
+            body: JSON.stringify({ userID: modalState?.user.id, schoolId: modalRequest?.school.id }),
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${authCode}`
+            }
+        }).then(response => {
+            if (!response.ok) {
+                throw Error(`Request rejected with status ${response.status}`)
+            }
+        }).catch(error => console.log(error))
 
-    const userGroup = (props.schoolId !== undefined && users !== undefined) ? users?.map((user: any, index: number) =>
+        fetch('https://localhost:44375/addToRole', {
+            method: 'POST',
+            body: JSON.stringify({ userID: modalState?.user.id, role: modalRequest?.role }),
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${authCode}`
+            }
+        }).then(response => {
+            if (!response.ok) {
+                throw Error(`Request rejected with status ${response.status}`)
+            }
+        }).catch(error => console.log(error))
+        handleUpdate()
+        setModalRequest(undefined)
+        setModalState({ active: undefined })
+    }
+
+    const schoolGroup = props.schools !== undefined ? props.schools?.map((school: any, index: number) =>
+        <li className="menu-item" onClick={() => {
+            setSchoolState({
+                schoolId: school.id,
+                schoolName: school.name
+            })
+        }} key={index}><a>{school.name}</a></li>) : <div>No avalible schools</div>
+
+    const schoolModalGroup = props.schools !== undefined ? props.schools?.map((school: any, index: number) =>
+        <li className="menu-item" onClick={() => {
+            setModalRequest({
+                school: school,
+                role: modalRequest?.role
+            })
+        }} key={index} > <a>{school.name}</a></li >) : <div>No avalible schools</div>
+
+    const userGroup = (schoolState?.schoolId !== undefined && users !== undefined) ? users?.map((user: any, index: number) =>
         <tr onClick={() => { setModalState({ active: 'active', user: user }) }} key={index}>
             <td>{user.username}</td>
             <td>{user.school}</td>
@@ -76,25 +118,51 @@ export const AdminPanel = (props: AdminPanelProps) => {
 
     return (
         <div>
-            <div className={`modal ${modalState.active} modal-sm`}>
+            <div className="dropdown">
+                <a href="#" className="btn btn-link dropdown-toggle" tabIndex={0}>
+                    {schoolState?.schoolName ? schoolState.schoolName : t('School')}
+                </a>
+                <ul className="menu">
+                    {schoolGroup}
+                </ul>
+            </div>
+            <button className="btn" onClick={handleUpdate}>Update</button>
+            <div className={`modal ${modalState?.active}`}>
                 <a href="#close" className="modal-overlay" aria-label="Close"></a>
-                <div ref={modalRef} className="modal-container">
+                <div className="modal-container actionModal">
                     <div className="modal-header">
-                        <a href="#close" onClick={() => { setModalState({ active: undefined }) }} className="btn btn-clear float-right" aria-label="Close" />
+                        <a href="#close" onClick={() => { setModalState({ active: undefined }); handleUpdate() }} className="btn btn-clear float-right" aria-label="Close" />
                         <div className="modal-title h5">{t('Action')}</div>
                     </div>
-                    <div className="modal-body">
+                    <div className="modal-body modalBody">
                         <div className="content">
-                            {modalState.user?.role}
-                            <br />
-                            {modalState.user?.school}
+                            <div className="dropdown">
+                                <a href="#" className="btn btn-link dropdown-toggle" tabIndex={0}>
+                                    {modalRequest?.school.name ? modalRequest.school.name : t('School')}
+                                </a>
+                                <ul className="menu">
+                                    {schoolModalGroup}
+                                </ul>
+                                <br />
+                            </div>
+                            <div className="dropdown">
+                                <a href="#" className="btn btn-link dropdown-toggle" tabIndex={0}>
+                                    {modalRequest?.role ? modalRequest?.role : t('Role')}
+                                    </a>
+                                <ul className="menu">
+                                    <li className="menu-item" onClick={() => { setModalRequest({ school: modalRequest?.school, role: 'Admin' }) }}><a>{t('Admin')}</a></li>
+                                    <li className="menu-item" onClick={() => { setModalRequest({ school: modalRequest?.school, role: 'SchoolAdmin' }) }}><a>{t('School Admin')}</a></li>
+                                    <li className="menu-item" onClick={() => { setModalRequest({ school: modalRequest?.school, role: 'User' }) }}><a>{t('User')}</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="modal-footer">
+                            <button className="btn" onClick={handleModalSubmit}>Submit</button>
                         </div>
                     </div>
-                    <div className="modal-footer">
-                        ...
-                    </div>
                 </div>
-            </div>
+            </div >
+
             <table className="table table-hover">
                 <thead>
                     <tr>
--- frontend/src/Pages/dashboard.tsx
+++ frontend/src/Pages/dashboard.tsx
@@ -3,6 +3,7 @@ import { Surveys } from './Surveys/surveySelection'
 import { AdminPanel } from './Admin Panel/adminPanel'
 import { useSelector, useDispatch } from 'react-redux'
 import useAPI from '../Services/API'
+import { useTranslation } from 'react-i18next'
 
 
 enum Roles {
@@ -11,30 +12,21 @@ enum Roles {
     User = 'User'
 }
 
+
+
 export const Dashboard = () => {
     const role = useSelector((state: any) => state.user.role)
-    const [schoolId, setSchoolId] = useState<string | undefined>(undefined)
+    const { t } = useTranslation()
     const schools = useAPI('schools')
     const dispatch = useDispatch()
 
     if (role === Roles.Admin || role === Roles.SchoolAdmin) {
         dispatch({ type: 'SET_PENDING_FALSE' })
 
-        const schoolGroup = schools !== undefined ? schools?.map((school: any, index: number) =>
-            <li className="menu-item" onClick={() => {
-                setSchoolId(school.id)
-            }} key={index}><a>{school.name}</a></li>) : <div>No avalible schools</div>
+        
         return (
             <div>
-                <div className="dropdown">
-                    <a href="#" className="btn btn-link dropdown-toggle" tabIndex={0}>
-                        {'Schools'}
-                    </a>
-                    <ul className="menu">
-                        {schoolGroup}
-                    </ul>
-                </div>
-                <AdminPanel schoolId={schoolId} />
+                <AdminPanel schools={schools} />
             </div>
         )
     } else if (role === Roles.User) {
